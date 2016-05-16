4887ae6becda48dc53746b0d6fb88877dedf39f3
Puzhu <hari.subhash@gmail.com>
2016-05-16 18:54:33 -0400

    catch-of-the-day
    For the react tutorial

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// Create a <StorePicker/>
+
+var StorePicker = React.createClass({
+  render: function() {
+    return (
+      <p>This is awesome. This rocks. I am loving atom and I am loving React</p>
+    )
+  }
+});
+
+ReactDOM.render(<StorePicker/>, document.getElementById('main'));
