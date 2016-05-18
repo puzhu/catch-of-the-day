d2d91e4c2ab12bc5754cd65d9dacb16454f8dc35
Puzhu <hari.subhash@gmail.com>
2016-05-18 13:22:12 -0400

    Setup the App component

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,55 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+/*
+  Lets create our primary app component. All the subcomponents will be nested within it
+*/
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className='catch-of-the-day'>
+        <div className='menu'>
+          <Header/>
+        </div>
+        <Order/>
+        <Inventory/>
+      </div>
+    )
+  }
+})
+
+/*
+  Create the Header component <Header/>
+*/
+var Header = React.createClass({
+  render: function(){
+    return (
+      <p>Header</p>
+    )
+  }
+})
+
+/*
+  Create the Order component <Order/>
+*/
+var Order = React.createClass({
+  render: function(){
+    return (
+      <p>Order</p>
+    )
+  }
+})
+/*
+  Create the Inventory component <Inventory/>
+*/
+var Inventory = React.createClass({
+  render: function(){
+    return (
+      <p>Inventory</p>
+    )
+  }
+})
+
 // Create a <StorePicker/>
 
 var StorePicker = React.createClass({
@@ -20,5 +69,5 @@ var StorePicker = React.createClass({
   }
 });
 
-ReactDOM.render(<StorePicker/>, document.getElementById('main'));
+ReactDOM.render(<App/>, document.getElementById('main'));
 //sdfsaf
